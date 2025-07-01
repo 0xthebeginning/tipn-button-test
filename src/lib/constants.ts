f13a305bd7c9f23c9b771e6747c4fe3c8d663b14
0xthebeginning <station.tech@outlook.com>
2025-06-30 18:14:07 -0700

    m

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,9 +5,9 @@ export const APP_PRIMARY_CATEGORY = process.env.NEXT_PUBLIC_MINI_APP_PRIMARY_CAT
 export const APP_TAGS = process.env.NEXT_PUBLIC_MINI_APP_TAGS?.split(',');
 export const APP_ICON_URL = `${APP_URL}/icon.png`;
 export const APP_OG_IMAGE_URL = `https://tipn-button-test.vercel.app/splash.png`;
-export const APP_SPLASH_URL = `$https://tipn-button-test.vercel.app/splash.png`;
+export const APP_SPLASH_URL = `https://tipn-button-test.vercel.app/splash.png`;
 export const APP_SPLASH_BACKGROUND_COLOR = "#f7f7f7";
-export const APP_BUTTON_TEXT = process.env.NEXT_PUBLIC_MINI_APP_BUTTON_TEXT;
+export const APP_BUTTON_TEXT = "Tip Dev";
 export const APP_WEBHOOK_URL = process.env.NEYNAR_API_KEY && process.env.NEYNAR_CLIENT_ID 
     ? `https://api.neynar.com/f/app/${process.env.NEYNAR_CLIENT_ID}/event`
     : `${APP_URL}/api/webhook`;
