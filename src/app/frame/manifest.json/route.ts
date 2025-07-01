ff4d54f301e0826b79d721535b5ca110564734a4
0xthebeginning <station.tech@outlook.com>
2025-06-30 17:06:48 -0700

    update manifest again..

--- src/app/frame/manifest.json/route.ts
+++ src/app/frame/manifest.json/route.ts
@@ -1,12 +1,20 @@
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
 export async function GET() {
   return NextResponse.json({
     name: "Tip Button Mini App",
     description: "Prototype tipping mini app for developers",
     icon: "https://tipn-button-test.vercel.app/favicon.ico",
-    url: "https://tipn-button-test.vercel.app"
+    url: "https://tipn-button-test.vercel.app",
+    button: {
+      title: "Tip Dev 💸",
+    },
+    action: {
+      type: "launch_frame",
+      name: "Tip Button",
+      url: "https://tipn-button-test.vercel.app",
+      splashImageUrl: "https://tipn-button-test.vercel.app/splash.png",
+      splashBackgroundColor: "#f7f7f7",
+    },
   });
 }
