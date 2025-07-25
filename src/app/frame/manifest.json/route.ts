be3638e808f77021812e58c0435bd3f09ce6990c
0xthebeginning <station.tech@outlook.com>
2025-07-24 17:48:47 -0700

    Update manifest to use superinu-miniapp.vercel.app

--- src/app/frame/manifest.json/route.ts
+++ src/app/frame/manifest.json/route.ts
@@ -3,19 +3,19 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json({
-    name: "Tip Button Mini App",
-    description: "Prototype tipping mini app for developers",
-    icon: "https://tipn-button-test.vercel.app/favicon.ico",
-    url: "https://tipn-button-test.vercel.app",
+    name: "SuperInu",
+    description: "SuperInu Miniapp",
+    icon: "https://superinu-miniapp.vercel.app/favicon.ico",
+    url: "https://superinu-miniapp.vercel.app",
     button: {
-      title: "Tip Dev",
+      title: "Make a SuperInu Moment 🐶✨",
     },
     action: {
       type: "launch_frame",
-      name: "Tip Button",
-      url: "https://tipn-button-test.vercel.app",
-      splashImageUrl: "https://tipn-button-test.vercel.app/splash.png",
-      splashBackgroundColor: "#f7f7f7",
+      name: "Make a SuperInu Moment 🐶✨",
+      url: "https://superinu-miniapp.vercel.app",
+      splashImageUrl: "https://superinu-miniapp.vercel.app/splash.png",
+      splashBackgroundColor: "#52a842",
     },
   }, {
     headers: {
