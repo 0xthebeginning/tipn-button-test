684a24c6820ee56b145807660b8fcbac9fe23a97
0xthebeginning <station.tech@outlook.com>
2025-07-24 21:24:49 -0700

    Se

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,19 +1,19 @@
 // src/app/page/metadata.ts
-import { Metadata } from "next";
-import { APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "~/lib/constants";
+//import { Metadata } from "next";
+//import { APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "~/lib/constants";
 //import { getMiniAppEmbedMetadata } from "~/lib/utils";
 
-export const generateMetadata = async (): Promise<Metadata> => {
-  return {
-    title: APP_NAME,
-    description: APP_DESCRIPTION,
-    openGraph: {
-      title: APP_NAME,
-      description: APP_DESCRIPTION,
-      images: [APP_OG_IMAGE_URL],
-    }//,
+//export const generateMetadata = async (): Promise<Metadata> => {
+//  return {
+//    title: APP_NAME,
+//    description: APP_DESCRIPTION,
+//    openGraph: {
+//      title: APP_NAME,
+//      description: APP_DESCRIPTION,
+//      images: [APP_OG_IMAGE_URL],
+//    }//,
 //    other: {
 //      "fc:frame": JSON.stringify(getMiniAppEmbedMetadata()),
 //    },
-  };
-};
+//  };
+//};
